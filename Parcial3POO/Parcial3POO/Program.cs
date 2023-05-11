87ba3516908f3a90ac56728160da7bc4fa9edcd0
Andy Morales <andyemmejia@gmail.com>
2023-05-11 15:43:45 -0500

    Prueba Commit

---
+++ Parcial3POO/Parcial3POO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Parcial3POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
