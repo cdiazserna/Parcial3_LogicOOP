7433fd5ef786cc6ad7f43a8c45c6dd02f5b5ff5e
JAIRO LEANDRO MORA GUERRA <jairomora214411@correo.itm.edu.co>
2023-05-11 13:49:49 -0500

    Prueba commit

---
+++ Parcial3_OPP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Parcial3_OPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
