5fa0fdf7c71d431c0cc041a1b647861d3e8c6304
Juan Pablo Rebolledo Almeida <rebolledoalmeida@hotmail.com>
2023-05-11 19:03:58 -0500

    Parcial 3

---
+++ Bank/Bank/BankAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class BankAccount
+{
+	public int numberAccount { get; set; }
+	public int holder { get; set; }
+	public string nameBank { get; set; }
+
+
+	public void deposit()
+	{
+
+	}
+
+	public void withDraw()
+	{
+
+	}
+
+}
---
+++ Bank/Bank/CheckingAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class CheckingAccount
+{
+    public int credit { get; set; }
+    public string interestRate { get; set; }
+    public string overDraft { get; set; }
+
+
+
+    public void deposit()
+    {
+
+    }
+
+    public void withDraw()
+    {
+
+    }
+
+    public void calculateOverdraft()
+    {
+
+    }
+
+    public void calculateLatePayment()
+    {
+
+    }
+}
