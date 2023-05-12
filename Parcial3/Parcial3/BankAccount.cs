cedb770c5cffd7c8dc9275190d694ef0fa8b808a
LEYDI YULIETH PUERTA CAMPO <leydipuerta249947@correo.itm.edu.co>
2023-05-11 21:23:10 -0500

    Proceso ejercicio

--- Parcial3/Parcial3/BankAccount.cs
+++ Parcial3/Parcial3/BankAccount.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
-public class Class1
+public class BankAccount
 {
-	public Class1()
+	public BankAccount()
 	{
 	}
 }
--- Parcial3/Parcial3/SavingAccount.cs
+++ Parcial3/Parcial3/SavingAccount.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
-public class Class1
+public class SavingAccount
 {
-	public Class1()
+	public SavingAccount()
 	{
 	}
 }
