49c7d11c7ed5c9359b0f9b9c6bf0b8c91fdb8395
Juan Pablo Rebolledo Almeida <rebolledoalmeida@hotmail.com>
2023-05-11 20:03:41 -0500

    parcial 3

--- Bank/Bank/SavingAccount.cs
+++ Bank/Bank/SavingAccount.cs
@@ -1,29 +1,38 @@
 ﻿using System;
 
-public class SavingAccount
+public class SavingAccount:BankAccount
 {
     public int yield { get; set; }
-    public int balance{ get; set; }
-
+    public decimal balance{ get; set; }
+    public decimal cashWithdrawn { get; set; }
+    public decimal trasfers { get; set; }
+    public int moths { get; set; }
 
 
     public void deposit()
     {
-
+        Console.WriteLine("Ingrese la cuenta del titular del credito \n");
+        numberAccount = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese El monto del credito \n");
+        balance = Convert.ToInt32(Console.ReadLine());
     }
 
     public void withdraw()
     {
-
+        Console.WriteLine("Ingrese la cantidad de dinero a retirar \n");
+        cashWithdrawn = Convert.ToDecimal(Console.ReadLine());
+        balance = balance - cashWithdrawn;
     }
 
     public void trasfer()
     {
-
+        Console.WriteLine("Ingrese la cantidad a traferir \n");
+        trasfers = Convert.ToDecimal(Console.ReadLine());
+        balance = balance - trasfers;
     }
 
-    public void calculateYield()
+    public decimal calculateYield()
     {
-
+        return moths * yield;
     }
 }
